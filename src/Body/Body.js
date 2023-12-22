bbc72fa5ddc9bd90613b5826cf4d73c39dcc0dcc
zach <zach.walnock@gmail.com>
2023-12-21 21:26:50 -0500

    Adding mobile support and fixed css

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,4 +1,5 @@
 import './Body.css'
+import selfPortrait from './Images/NittanyAiHeadShot4.JPG'
 const Body = () => {
     const informationalText = `I'm a student at Penn State, double majoring in computer science and math. I built this website to help strengthen my front end development skills.
     I've had plenty of project ideas for some time now, but I found myself with no Ui/Ux experience to develop them. I find that I learn the best by doing, so what better way to learn
@@ -9,7 +10,9 @@ const Body = () => {
                 <div className="headerText">Hi, I'm Zach.</div>
                 <div className='bodyText'>{informationalText}</div>
             </div>
-            <div className="bigImage"></div>
+            <div className='imageContainer'>
+                <img className="bigImage" src={selfPortrait} alt="Self Portrait"></img>
+            </div>
         </div>
     )
 }
